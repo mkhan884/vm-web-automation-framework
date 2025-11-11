c974ef1389a555b9a8487b4706bd7d7d79660787
mkhan884 <mkwestern1@gmail.com>
2025-11-11 14:39:17 +0400

    Fixing home internet label

--- pages/service_type_page.py
+++ pages/service_type_page.py
@@ -9,7 +9,7 @@ class ServiceTypePage(BasePage):
     
     SERVICE_TYPE_PAGE_HEADING = (By.XPATH, "//h1[text()='What brings you here?']")
     MOBILE_PLAN_BTN = (By.XPATH, "//a[p[normalize-space(.)='Mobile Plan']]")
-    HOME_INTERNET_BTN = (By.XPATH, "//a[p[normalize-space(.)='Home Internet']]")
+    HOME_INTERNET_BTN = (By.XPATH, '//p[normalize-space(text())="Home Internet" and span[normalize-space(.)="Emirates ID holders only"]]')
     TOURIST_BTN = (By.XPATH, "//p[contains(text(), 'Tourist Plan') and span[text()='Passport only']]")
 
     @allure.step("Verify that the service type page has loaded correctly")
@@ -24,7 +24,8 @@ class ServiceTypePage(BasePage):
     
     @allure.step("Select MBB plan in the sign up flow")
     def click_mbb_plan_btn(self):
-        self.scroll_to(self.HOME_INTERNET_BTN).click()
+        self.scroll_to(self.HOME_INTERNET_BTN)
+        self.click(self.HOME_INTERNET_BTN)
         return CoverageCheckPage(self.driver)
     
     @allure.step("Select tourist plan in the sign up flow")
