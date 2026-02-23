43d42d6b1e936b1b0f009b5a4d0e6036442ce3d4
Muhammad Khan <mkwestern1@gmail.com>
2026-02-23 10:30:58 +0400

    TC001 and TC002 Fix

--- pages/activation_method_page.py
+++ pages/activation_method_page.py
@@ -7,24 +7,26 @@ import allure
 class ActivationMethodPage(BasePage):
 
     PAGE_HEADER = (By.XPATH, "//span[normalize-space(.)='Choose activation method']")
-    STORE_ACTIVATION_CARD = (By.XPATH, "//div[contains(@class, 'activation-method-option') and contains(@class, 'instore-activation')]")
-    STORE_ACTIVATION_BTN = (By.XPATH, "//button[text()='Physical Activation']")
-    DRIVER_BTN = (By.XPATH, "//button[normalize-space(.)='Activation via driver']")
+    DRIVER_CARD = (By.XPATH, "//p[@class='activation-method-title' and normalize-space(.)='Activation via Driver']")
+    DELIVERY_ACTIVATION_BTN = (By.XPATH, "//button[text()='delivery activation']")
+    STORE_ACTIVATION_LINK = (By.XPATH,"//button[contains(@class,'instore-activation-method-cta') and normalize-space(.)='Activation in store']")
+
 
     @allure.step("Verify the activation method page header, instant activation / store activation cards.")
     def verify_activation_method_page(self):
         header = self.find(self.PAGE_HEADER)
-        store = self.scroll_to(self.STORE_ACTIVATION_CARD)
-        driver = self.scroll_to(self.DRIVER_BTN)
+        driver = self.find(self.DRIVER_CARD)
+        store = self.find(self.STORE_ACTIVATION_LINK)
+
         assert header.is_displayed() and store.is_displayed() and driver.is_displayed()
 
-    @allure.step("Select in store activation button and navigate to payment page")
+    @allure.step("Select activation via driver button and navigate to address page.")
     def select_driver_activation(self):
-        self.scroll_to(self.DRIVER_BTN).click()
+        self.scroll_to(self.DELIVERY_ACTIVATION_BTN).click()
         return AddressPage(self.driver)
     
+    @allure.step("Select activation in store link and navigate to checkout page.")
     def select_store_activation(self):
-        self.scroll_to(self.STORE_ACTIVATION_BTN)
-        self.click(self.STORE_ACTIVATION_BTN)
+        self.click(self.STORE_ACTIVATION_LINK)
         return CheckoutPage(self.driver)
 
--- pages/mbb_promo_page.py
+++ pages/mbb_promo_page.py
@@ -5,9 +5,9 @@ import allure
 
 class MbbPromoPage(BasePage):
 
-    PAGE_HEADER = (By.XPATH, '//h2[@class="vmo-page-title" and normalize-space(text())="12-months offer"]')
+    PAGE_HEADER = (By.XPATH, '//h1[text()="Limited time offer"]')
     CARD_TITLE = (By.XPATH, '//h3[normalize-space(.)="Home Internet for 12 months"]')
-    ROUTER_IMG = (By.XPATH, '//img[@alt="Huawei 5G Router"]')
+    ROUTER_IMG = (By.XPATH, "//h3[contains(.,'Home Internet for') and contains(.,'12 months')]")
     CREATE_PLAN_HEADER = (By.XPATH, '//span[normalize-space(text())="Create your own plan"]')
     DATA_CARD = (By.XPATH, '//div[contains(@class, "vmo-broadband-plan-data") and contains(@class, "vmo-broadband-plan-card")]')
     ROUTER_CARD = (By.XPATH, '//div[contains(@class, "vmo-broadband-plan-modem") and contains(@class, "vmo-broadband-plan-card")]')
@@ -16,12 +16,13 @@ class MbbPromoPage(BasePage):
     CONTINUE_BTN = (By.XPATH, '//button[normalize-space(.)="Continue"]')
     COMPATIBILITY_CARD = (By.XPATH, '//p[@class="router-disclaimer-box-title bold" and normalize-space(.)="Avoid compatibility issues"]')
     CONTIUNE_WITHOUT_ROUTER = (By.XPATH, '//button[normalize-space(.)="Continue without Router"]')
+    ONE_MONTH_TAB = (By.XPATH, "//div[normalize-space()='1 month']")
 
     @allure.step("Verify that the header, title and router image have loaded")
     def verify_page_load(self):
-        title = self.find(self.CARD_TITLE)
-        img = self.find(self.ROUTER_IMG)
-        assert title.is_displayed() and img.is_displayed()
+        title = self.find(self.PAGE_HEADER)
+        # img = self.find(self.ROUTER_IMG)
+        assert title.is_displayed()
     
     @allure.step("Verify header, data card and router card. Select no router and proceed")
     def choose_plan(self):
@@ -29,6 +30,7 @@ class MbbPromoPage(BasePage):
         data_card = self.find(self.DATA_CARD)
         router_card = self.find(self.ROUTER_CARD)
         assert header.is_displayed() and data_card.is_displayed() and router_card.is_displayed()
+        self.scroll_to(self.ONE_MONTH_TAB).click()
         self.scroll_to(self.NO_ROUTER_TAB).click()
         self.scroll_to(self.PLAN_PRICE)
         price_text = self.get_text(self.PLAN_PRICE)
